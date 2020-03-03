96898fbabed887090edca4e71abb057e82ae5536
Amir <amir.ibragimov13@mail.ru>
2020-03-04 01:58:12 +0300

    slider without adaptive

--- js/main.js
+++ js/main.js
@@ -1,45 +1,17 @@
 $('.owl-carousel').owlCarousel({
-    stagePadding: 192,
     loop:true,
     margin:48,
-    nav:false,
+    nav:true,
     dots:false,
+    center:true,
+    autoWidth:true,
+    items:1,
     responsive:{
         0:{
-            items:1,
-            stagePadding: 0,
             dots:true
         },
-        600:{
-            items:1,
-            stagePadding: 0,
-            nav:true,
-            dots:true
-        },
-        1000:{
-            items:1,
-            dots:true
-        },
-        1350:{
-            items:1,
-            stagePadding:250
-        },
-        1600:{
-            items:1,
-            stagePadding:350
-        }, 
-        1980:{
-            items:1,
-            stagePadding:450
-        }               
+        1200:{
+            dots:false
+        },               
     }
-});
-var owl=$(".owl-carousel");
-owl.owlCarousel();
-//$(".next") - находим нашу кнопку
-$(".next").click(function(){
-    owl.trigger("next.owl.carousel");
-});
-$(".prev").click(function(){
-    owl.trigger("prev.owl.carousel");
 });
